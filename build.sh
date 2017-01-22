fccbabfdf77775420a35e028eacea98690ecdb1d
James Bloom <jamesdbloom@gmail.com>
2017-01-22 09:00:31 +0000

    cleaning up build output

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 bundle install
-mvn -Dmockserver.serverPort=8098 -Dmockserver.proxyPort=9102 -Dmockserver.logLevel=INFO org.mock-server:mockserver-maven-plugin:3.10.4:runForked
+mvn -q -Dmockserver.serverPort=8098 -Dmockserver.proxyPort=9102 -Dmockserver.logLevel=INFO org.mock-server:mockserver-maven-plugin:3.10.4:runForked
 bundle exec rake spec
-mvn -Dmockserver.serverPort=8098 -Dmockserver.proxyPort=9102 -Dmockserver.logLevel=INFO org.mock-server:mockserver-maven-plugin:3.10.4:stopForked
+mvn -q -Dmockserver.serverPort=8098 -Dmockserver.proxyPort=9102 -Dmockserver.logLevel=INFO org.mock-server:mockserver-maven-plugin:3.10.4:stopForked
